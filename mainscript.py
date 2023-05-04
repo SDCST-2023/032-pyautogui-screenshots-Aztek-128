87eaa1a143d38618eeebd8487f8326acad21142b
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-05-04 11:33:51 -0700

    yes

--- mainscript.py
+++ mainscript.py
@@ -2,8 +2,8 @@ import pyautogui as p
 import time
 
 #debug
-numclicks = 10
-
+numclicks = 100
+p.PAUSE = 0.1
 
 
 def clicks():
@@ -16,13 +16,43 @@ def clicks():
             p.click(x)
             y = y+1
 
-
 def upgrade():
-    store = p.locateCenterOnScreen('assets/store.png')
+    store = p.locateCenterOnScreen('assets/store.png',confidence = 0.7)
     x = store[0]
     y = store[1]
     p.moveTo(x-130,y+50)
     p.click()
+
+def purchase():
+    p.moveTo(1303,458)
+    p.scroll(-200)
+
+
+    cursor = p.locateCenterOnScreen('assets/cursor.png')
+
+    if cursor != None:
+        time.sleep(1)
+        xcoords = cursor[0]
+        ycoords = cursor[1]
+        dy = 70
+
+        while True:
+            p.click(xcoords,ycoords-100,2)
+            ycoords = ycoords+dy
+            if ycoords >970:
+                break
+
+    time.sleep(1)
+    p.scroll(300)        
+
+
+'''def gold():
+    while True:
+        golden = p.locateCenterOnScreen('assets/goldencookie.png')
+        if golden !=None:
+            p.click(golden)'''
+
+
 
 
 def main():
@@ -30,6 +60,8 @@ def main():
     p.hotkey('ctrl','win','right')
     while True:
         clicks() 
-        upgrade()   
+        upgrade()  
+        purchase() 
+        #gold()
 
 main()
--- practicesystem.py
+++ practicesystem.py
@@ -1,21 +1,9 @@
 import pyautogui as p
 import time
 time.sleep(3)
-p.scroll(-220)
-
-cursor = p.locateCenterOnScreen('assets/cursor.png')
-
-if cursor != None:
-    time.sleep(3)
-    xcoords = cursor[0]
-    ycoords = cursor[1]
-    dy = 70
-
-    while True:
-        p.click(xcoords,ycoords-250,1)
-        ycoords = ycoords+dy
-
-time.sleep(3)
 
 
-p.scroll(300)
+while True:
+    golden = p.locateCenterOnScreen('assets/goldencookie.png')
+    print(golden)
+    
