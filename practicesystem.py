8f511ca587cfdfae96c90ba95710abebd8d72d67
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-05-05 11:34:04 -0700

    yes

--- practicesystem.py
+++ practicesystem.py
@@ -7,4 +7,11 @@ time.sleep(3)
 while True:
     golden = p.locateCenterOnScreen('assets/goldencookie.png',confidence = 0.5)
     print(golden)
-    p.click(golden)
+    if golden!= None:
+        p.click(golden)
+
+    while True:
+        golden = p.locateCenterOnScreen('assets/golden2cookie.png',confidence = 0.5)
+        print(golden)
+        if golden!= None:
+            p.click(golden)
